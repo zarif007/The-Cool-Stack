210628e5ad3a261e448cece03926cab5c6b136fe
zarif007 <zarifhuq786@gmail.com>
2023-09-27 01:21:02 +0600

    [CONNECT]: trpc with db

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,17 @@
+import Database from "better-sqlite3";
 import { publicProcedure, router } from "./trpc";
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
+import { prompts } from "@/db/schema";
+import { drizzle } from 'drizzle-orm/better-sqlite3'
+
+const sqlite = new Database("sqlite.db");
+const db = drizzle(sqlite)
+migrate(db, {migrationsFolder: "drizzle"})
 
 export const appRouter = router({
     // api/trpc/getPrompts
     getPrompts: publicProcedure.query(async () => {
-        return [{ user: "123", prompt: "I am Pokemon", response: "Charizard" }, { user: "123", prompt: "I love pikachu", reponse: "Pikachu" }]
+        return await db.select().from(prompts).all()
     })
 })
 
